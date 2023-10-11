1d327d241c6ed79608feb776a9a10a98745cc7de
Felipe Velandia <fvelandia695@gmail.com>
2023-10-10 23:34:59 -0500

    TIPODOC nombre

--- controllers/tipoDoc.controller.js
+++ controllers/tipoDoc.controller.js
@@ -2,21 +2,21 @@ const utilities = require('../config/utilities');
 const controller = {};
 
 controller.getTipoDocs = async (req, res) => {
-  const query = `SELECT * FROM TIPO_DOC`;
+  const query = `SELECT * FROM TIPODOC`;
   const result = await utilities.executeQuery(query);
   res.json(result);
 };
 
 controller.getTipoDocById = async (req, res) => {
   const { idtipodoc } = req.params;
-  const query = `SELECT * FROM TIPO_DOC WHERE idtipodoc = ${idtipodoc}`;
+  const query = `SELECT * FROM TIPODOC WHERE idtipodoc = ${idtipodoc}`;
   const result = await utilities.executeQuery(query);
   res.json(result);
 };
 
 controller.createTipoDoc = async (req, res) => {
   const { idtipodoc, desctipodoc } = req.body;
-  const query = `INSERT INTO TIPO_DOC VALUES (${idtipodoc}, '${desctipodoc}')`;
+  const query = `INSERT INTO TIPODOC VALUES (${idtipodoc}, '${desctipodoc}')`;
   const result = await utilities.executeQuery(query);
   res.json(result);
 };
@@ -26,14 +26,14 @@ controller.updateTipoDoc = async (req, res) => {
   const { desctipodoc } = req.body;
   const changeString = [];
   if (desctipodoc) changeString.push(`desctipodoc = '${desctipodoc}'`);
-  const query = `UPDATE TIPO_DOC SET ${changeString.join(', ')} WHERE idtipodoc = ${idtipodoc}`;
+  const query = `UPDATE TIPODOC SET ${changeString.join(', ')} WHERE idtipodoc = ${idtipodoc}`;
   const result = await utilities.executeQuery(query);
   res.json(result);
 };
 
 controller.deleteTipoDoc = async (req, res) => {
   const { idtipodoc } = req.params;
-  const query = `DELETE FROM TIPO_DOC WHERE idtipodoc = ${idtipodoc}`;
+  const query = `DELETE FROM TIPODOC WHERE idtipodoc = ${idtipodoc}`;
   const result = await utilities.executeQuery(query);
   res.json(result);
 };
